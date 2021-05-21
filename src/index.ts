093fe53dec86f4b55943b4109bc1cfd89bb397e1
JaejinHwang <hjj4756@kaist.ac.kr>
2021-05-21 13:37:33 +0900

    understanding interface
    understanding concept of interface feature

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
-const name = "Jay",
-    age = 26,
-    gender = "male";
-
-const sayHi = (name: string, age: number, gender: string): string => {
-    return `Hello ${name}, you are ${age}, you are a ${gender}!!`;
+interface Human {
+    name: string
+    age: number
+    gender: string
 };
 
-console.log(sayHi(name, age, gender));
+const person = {
+    name: "Jay",
+    age: 26,
+    gender: "male"
+};
+
+const sayHi = (person: Human): string => {
+    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!!`;
+};
+
+console.log(sayHi(person));
 
 export {};
